5072ce540ea1ca315c46bd8fc24a118da2db0965
Max R <mxr@users.noreply.github.com>
2022-04-07 09:38:08 -0400

    Print solution output after headings

--- _common.py
+++ _common.py
@@ -18,8 +18,10 @@ def main(part1: Callable[[str], int], part2: Callable[[str], int]) -> int:
     filename: str = args.filename
 
     if (part or 1) == 1:
-        print(f"part1: {part1(filename)}")
+        print("part1: ", end="")
+        print(part1(filename))
     if (part or 2) == 2:
-        print(f"part2: {part2(filename)}")
+        print("part2: ", end="")
+        print(part2(filename))
 
     return 0
